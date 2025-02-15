234b35303973c1b2b038d074ab832ac3d759ead6
Sandro José Rodrigues Coelho <sandrojoserodriguescoelho@gmail.com>
2025-02-15 02:54:33 +0000

    Finished the setup for the first mino
    Completed mino L1 moveset, and changed mino class functions

--- TetrisInJava/src/main/KeyHandler.java
+++ TetrisInJava/src/main/KeyHandler.java
@@ -15,6 +15,21 @@ public class KeyHandler implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
 
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+        if (code == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+        if (code == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+        if (code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+
     }
 
     @Override
